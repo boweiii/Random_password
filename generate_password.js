cb233318e1b8acb49f2939dd5e2a9d0266c66038
Owen <z32498926@gmail.com>
2021-07-08 11:13:10 +0800

    Create generate_password.js & define things user might want

---
+++ generate_password.js
@@ -0,0 +1,19 @@
+// generate_password.js
+// define generatePassword function
+function generatePassword() {
+  // define things user might want
+  const lowerCaseLetter = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const numbers = '0123456789'
+  const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
+  // create a collection to store things user picked up
+
+  // remove things user do not need
+
+  // start generating password
+
+  // returnr the generated password
+  console.log('This function will generate password')
+}
+// invoke generatePassword function
+generatePassword()
