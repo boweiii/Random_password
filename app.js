cfac6af6c9c26053c0665b08e77b7abdfd1f5dc9
Owen <z32498926@gmail.com>
2021-07-08 10:52:28 +0800

    Used body-parser

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 // require related modules used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
+// const bodyParser = require('body-parser')  //Express 版本升級 4.17.1 後，已內建 body-parser,故可省略
 const app = express()
 const port = 3000
 
-// set templat engine and bootstrap
+// set templat engine and bootstrap and body-parser
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+// app.use(bodyParser.urlencoded({ extended: true })) //Express 版本升級 4.17.1 後，已內建 body-parser，故可省略
+app.use(express.urlencoded({ extended: true }))   //這行程式碼擺放的順序需要在所有路由設定之前。
 
 // localhost : 3000
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('req.body', req.body)
   res.render('index')
 })
 
